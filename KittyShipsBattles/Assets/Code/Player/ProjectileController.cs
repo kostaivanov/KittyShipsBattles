87efa1131ee57836e879b04bc2036a932f5444a2
Kosta Ivanov <kostasvetlozarov@gmail.com>
2023-05-04 01:15:52 +0300

    Update PlayerMovement.cs, ProjectileController.cs, and 2 more files...

--- KittyShipsBattles/Assets/Code/Player/ProjectileController.cs
+++ KittyShipsBattles/Assets/Code/Player/ProjectileController.cs
@@ -10,57 +10,95 @@ public class ProjectileController : MonoBehaviour
     public float maxInitialSpeed = 20f;
     public float maxDistance = 100f;
     public Vector2 gravity = new Vector2(0, -9.81f);
-    private Vector2 initialMousePosition;
+    private Vector3 initialMousePosition;
     private Vector2 velocity;
     private bool isDragging = false;
 
     private TrajectoryLine trajectoryLine;
 
+    private PlayerMovement playerMovement;
+    private Camera mainCamera;
+    private float mouseDownTime;
+    [SerializeField] private float maxClickDuration; // Maximum time the mouse button can be held down for a click to be registered
+    [SerializeField] private float minDragDistance; // Maximum time the mouse button can be held down for a click to be registered
+
     private void Start()
     {
+        mainCamera = Camera.main;
         trajectoryLine = GetComponent<TrajectoryLine>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
 
     void Update()
     {
-        // Get mouse position in world coordinates
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         // Detect mouse button press and start dragging
         if (Input.GetMouseButtonDown(0))
         {
-            initialMousePosition = mousePosition;
+            initialMousePosition = Input.mousePosition;
             isDragging = true;
+            mouseDownTime = Time.time;
         }
         // Detect mouse button release and shoot projectile
         else if (Input.GetMouseButtonUp(0) && isDragging)
         {
+            float clickDuration = Time.time - mouseDownTime;
+            Vector3 mouseUpPosition = Input.mousePosition;
+            float dragDistance = Vector2.Distance(initialMousePosition, mouseUpPosition);
+
             // Calculate shooting power based on the mouse movement
-            float shootingPower = Mathf.Clamp01(Vector2.Distance(initialMousePosition, mousePosition) / maxDistance);
+            float shootingPower = Mathf.Clamp01(dragDistance / maxDistance);
             float initialSpeed = Mathf.Lerp(minInitialSpeed, maxInitialSpeed, shootingPower);
 
-            // Calculate velocity based on the mouse movement direction
-            velocity = (initialMousePosition - mousePosition).normalized * initialSpeed;
+            // Get mouse position in world coordinates
+            Vector3 clickedWorldPosition = mainCamera.ScreenToWorldPoint(mouseUpPosition);
+            clickedWorldPosition.z = 0;
 
-            // Instantiate projectile and set its initial position and velocity
-            GameObject projectile = Instantiate(projectilePrefab, muzzle.transform.position, Quaternion.identity);
+            Vector3 viewportPosition = mainCamera.WorldToViewportPoint(clickedWorldPosition);
 
-            //Set the shooting palyer reference on the projectile
-            //projectile.GetComponent<Projectile>().SetShootingPlayer(this.gameObject);
-            projectile.GetComponent<Projectile>().shootingPlayer = this.gameObject;
+            Debug.Log("Shooting power - " + shootingPower);
+            if (clickDuration < maxClickDuration && dragDistance < minDragDistance / maxDistance &&
+            viewportPosition.x >= 0 && viewportPosition.x <= 1 && viewportPosition.y >= 0 && viewportPosition.y <= 1)
+            {
+                // Move player to the clicked position
+                playerMovement.MovePlayer();
+            }
+            else
+            {
+                // Shoot the projectile
+                ShootProjectile(mouseUpPosition, initialSpeed);
+            }
 
-            projectile.GetComponent<Projectile>().Initialize(velocity, maxDistance, gravity);
-
-            // Reset dragging state
             isDragging = false;
             trajectoryLine.EndLine();
         }
 
         if (Input.GetMouseButton(0))
         {
-            Vector3 currentPoint = mousePosition;
-            trajectoryLine.RenderLine(initialMousePosition, currentPoint);
+            Vector3 currentPoint = Input.mousePosition;
+            Vector3 initialWorldPosition = mainCamera.ScreenToWorldPoint(initialMousePosition);
+            initialWorldPosition.z = 0;
+            Vector3 currentWorldPosition = mainCamera.ScreenToWorldPoint(currentPoint);
+            currentWorldPosition.z = 0;
+            trajectoryLine.RenderLine(initialWorldPosition, currentWorldPosition);
         }
+    }
+
+    private void ShootProjectile(Vector3 mouseUpPosition, float initialSpeed)
+    {
+        // Calculate velocity based on the mouse movement direction
+        velocity = (initialMousePosition - mouseUpPosition).normalized * initialSpeed;
+
+        // Instantiate projectile and set its initial position and velocity
+        GameObject projectile = Instantiate(projectilePrefab, muzzle.transform.position, Quaternion.identity);
+
+        //Set the shooting palyer reference on the projectile
+        //projectile.GetComponent<Projectile>().SetShootingPlayer(this.gameObject);
+        projectile.GetComponent<Projectile>().shootingPlayer = this.gameObject;
+
+        projectile.GetComponent<Projectile>().Initialize(velocity, maxDistance, gravity);
+
+        // Reset dragging state
+
     }
 }
